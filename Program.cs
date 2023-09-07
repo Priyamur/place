f092cf4a6a61b57a5f96dbe78f18fbe9e121ac3e
Priyadharshini Murugan <priyadharshini.mu@itp.objectfrontier.com>
2023-09-07 14:34:02 +0530

    first release

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+public abstract class TicketBooking()
+{
+    public abstract void RedBus();
+    public abstract void MovieShow();
+    public abstract void TrainTicket();
+    {
+        Console.WriteLine("train timings");
+        console.WriteLine("train name");
+        Console.WriteLine("train no");
+    }
+}
