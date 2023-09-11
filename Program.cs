725b560f83d0f60b2aa714ec8f79aba752b8599f
Priyadharshini Murugan <priyadharshini.mu@itp.objectfrontier.com>
2023-09-11 12:32:57 +0530

    ss

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-public abstract class TicketBooking()
+public abstract class TicketBooking
 {
     public abstract void RedBus();
     public abstract void MovieShow();
-    public abstract void TrainTicket();
+    public void TrainTicket()
     {
         Console.WriteLine("train timings");
-        console.WriteLine("train name");
+        Console.WriteLine("train name");
         Console.WriteLine("train no");
     }
+}
+public class Booking:TicketBooking
+{
+    public override void RedBus()
+    {
+        Console.WriteLine("bus details");
+        Console.WriteLine("bus no");
+        Console.WriteLine("passenger details");
+    }
+    public override void MovieShow()
+    {
+        Console.WriteLine("movie details");
+        Console.WriteLine("theater details");
+    }
+}
+class Program
+{
+    public static void Main(string [] args)
+    {
+        TicketBooking myticket = new Booking();
+        myticket.RedBus();
+        myticket.MovieShow();
+    }
+
 }
